7b40311e589af5af1cc75ebdf3a3f7fe77310886
GonzaloAzzad <azzadgonzalo@gmail.com>
2026-04-13 14:38:32 -0300

    Ejercicio 2: condicionales v2

--- ejercicios/c03-javascript/ej2-condicionales/app.js
+++ ejercicios/c03-javascript/ej2-condicionales/app.js
@@ -13,6 +13,31 @@ const clasificarNota = (nota) => {
     }
 }
 
+const diaDeLaSemana = (num) => {
+    switch(num){
+        case 1: console.log("Lunes");
+        break;
+        case 2: console.log("Martes");
+        break;
+        case 3: console.log("Miercoles");
+        break;
+        case 4: console.log("Jueves");
+        break;
+        case 5: console.log("Viernes");
+        break;
+        case 6: console.log("Sabado - Fin de semana");
+        break;
+        case 7: console.log("Domingo - Fin de semana");
+        break;
+        default: console.log("Día inválido");
+    }
+}
+
 clasificarNota(3);
 clasificarNota(6);
 clasificarNota(9);
+
+diaDeLaSemana(2);
+diaDeLaSemana(4);
+diaDeLaSemana(6);
+diaDeLaSemana(8);
